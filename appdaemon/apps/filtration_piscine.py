8ee7c54b833b6c8775ee2956206d17d4c9331651
Luis Fernando ROSA <luisfernando.rosa@iea.org>
2022-11-13 23:39:48 +0100

    correction affichage periode de filtration (suppression dernier ':').

--- appdaemon/apps/filtration_piscine.py
+++ appdaemon/apps/filtration_piscine.py
@@ -8,6 +8,9 @@ import hassapi as hass
 # Saisir ici les memes modes que dans HA 
 TAB_MODE = ["Ete", "Hiver", "At F", "Ma F"]
 
+# TODO: remplacer en_heure (str) par des timedelta
+# TODO: généraliser la gestion des modes de fonctionnement (une fonction pour chaque mode)
+
 
 class FiltrationPiscine(hass.Hass):
     """
@@ -263,9 +266,8 @@ class FiltrationPiscine(hass.Hass):
         # Flag FIN_TEMPO
         self.logger.debug(f"Flag fin tempo = {self.fin_tempo}")
         # Temporisation avant prise en compte de la mesure de la temperature
-        # sinon on travaille avec la memoire de la 
-        # temperature avant arret de la pompe
-        # mémorise la température eau de la veille.
+        # sinon on travaille avec la memoire de la temperature avant arrêt de la pompe
+        # mémorisée la veille.
         if self.fin_tempo:
             temperature_eau = mesure_temperature_eau
             self.set_value(self.args["mem_temp"], mesure_temperature_eau)
@@ -309,7 +311,7 @@ class FiltrationPiscine(hass.Hass):
             self.logger.info(f"Nb h_avant_t: {h_avant_t}/Nb h_apres_t: {h_apres_t}/Nb h_total_t: {h_total_t}")
             self.logger.info(f"h_debut: {h_debut}/h_pivot: {h_pivot}/h_fin: {h_fin}")
             # Affichage plage horaire
-            affichage_texte = str(h_debut)[:5] + "/" + str(h_pivot)[:5] + "/" + str(h_fin)[:5]
+            affichage_texte = f"{str(h_debut)[:5]:0>8}/{str(h_pivot)[:5]:0>8}/{str(h_fin)[:5]:0>8}"
             self.set_textvalue(periode_filtration, affichage_texte)
             # Ajouté le 30 mai 2022
             self.set_value("input_number.duree_filtration_ete", round(temps_filtration, 2))
@@ -332,7 +334,7 @@ class FiltrationPiscine(hass.Hass):
             duree_t = timedelta(hours=int(duree_h[:2]), minutes=int(duree_h[3:5]))
             h_fin_f = h_debut_t + duree_t
             # Affichage plage horaire
-            affichage_texte = str(h_debut_h)[:5] + "/" + str(h_fin_f)[:5]
+            affichage_texte = f"{str(h_debut_h)[:5]:0>8}/{str(h_fin_f)[:5]:0>8}"
             self.set_textvalue(periode_filtration, affichage_texte)
 
             self.logger.debug(f"h_debut_h: {h_debut_h}, Duree H: {duree_h}, H_fin: {h_fin_f}")
@@ -354,7 +356,7 @@ class FiltrationPiscine(hass.Hass):
             self.logger.warning(f"Mode de fonctionnement Piscine Inconnu: {mode_de_fonctionnement}")
 
         # Calcul sortie commande pompe filtration
-        # Arret pompe sur arret forcé
+        # Arrêt pompe sur arrêt forcé
         if arret_force == "on":
             self.turn_off(pompe)
             self.logger.info("Arrêt par Deléstage (Forcé)")
