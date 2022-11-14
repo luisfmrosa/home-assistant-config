f12272dbbdcfd0cfa3ef9adad1ec935c70e0782c
Luis Fernando ROSA <luisfernando.rosa@iea.org>
2022-11-14 20:00:46 +0100

    Généralisation de la gestion du mode de fonctionnement.

--- appdaemon/apps/filtration_piscine.py
+++ appdaemon/apps/filtration_piscine.py
@@ -1,14 +1,16 @@
 # Version du 30/05/2022
 import time
 from datetime import timedelta
+from typing import Callable, NoReturn
 
 import hassapi as hass
 
-# Variables globales
-# Saisir ici les memes modes que dans HA 
-TAB_MODE = ["Ete", "Hiver", "At F", "Ma F"]
 
-# TODO: généraliser la gestion des modes de fonctionnement (une fonction pour chaque mode)
+class UnknownModeError(KeyError):
+    """
+    Erreur: Mode de fontionnement inconnu.
+    """
+    pass
 
 
 class FiltrationPiscine(hass.Hass):
@@ -41,6 +43,9 @@ class FiltrationPiscine(hass.Hass):
     abaque_c: float = 2.43489
     abaque_d: float = -10.72859
 
+    # Registre associant des noms de mode de fonctionnement a des fonctions
+    registre_mode: dict = {}
+
     def initialize(self):
         """
         Initialise la classe, executé au démarrage de la classe.
@@ -55,6 +60,13 @@ class FiltrationPiscine(hass.Hass):
         # Arret de la pompe sur initalisation
         self.turn_off(self.args["cde_pompe"])
 
+        # enregistre les modes de fonctionnement connus
+        self.registre_mode_action('Ete', self.mode_ete)
+        self.registre_mode_action('Hiver', self.mode_hiver)
+        self.registre_mode_action('At F', self.mode_at_force)
+        self.registre_mode_action('Ma F', self.mode_ma_force)
+
+        # définie les actions à exécuter si changement dans variables
         self.listen_state(self.change_temp, self.args["temperature_eau"])
         self.listen_state(self.change_mode, self.args["mode_de_fonctionnement"])
         self.listen_state(self.change_coef, self.args["coef"])
@@ -69,6 +81,28 @@ class FiltrationPiscine(hass.Hass):
         # Lance le traitement a chaque self.intervale_minutes (minutes)
         self.logger.debug(f'Ajout temporisateur pour exécuter le traitement toutes les {self.intervale_minutes} min.')
         self.run_every(self.traitement, "now", self.intervale_minutes * 60)
+
+    def registrer_mode_action(self, mode_fonctionnement: str, action: Callable[[], bool]) -> NoReturn:
+        """
+        Cette méthode associe un nom de mode de fonctionnement à une fonction qui execute ce mode.
+        :param mode_fonctionnement: str texte avec le nom du mode de fonctionnement.
+        :param action: Callable[[], bool] une fonction qui execute ce mode de fonctionnement.
+        :return: NoReturn
+        """
+        self.registre_mode[mode_fonctionnement] = action
+
+    def get_mode_action(self, mode_fonctionnement: str) -> Callable[[], bool]:
+        """
+        Retourne l'action associée au mode de fonctionnement.
+        Remonte une exception UnknownModeError si mode inconnu.
+        :param mode_fonctionnement: str avec le libellée du mode de fonctionnement.
+        :return: Callable[[], bool] l'action associée au mode de fonctionnement.
+        """
+        try:
+            action: Callable[[], bool] = self.registre_mode[mode_fonctionnement]
+            return action
+        except KeyError:
+            raise UnknownModeError(f'Mode de fonctionnement unconnu: {mode_fonctionnement}')
 
     @staticmethod
     def duree_classique(temperature_eau: float):
@@ -230,22 +264,17 @@ class FiltrationPiscine(hass.Hass):
         self.logger.debug(f'Appel changement tempo circulation eau. Flag fin tempo: {self.fin_tempo}')
         self.initialize()
 
-    def traitement(self, kwargs):
+    def mode_ete(self) -> bool:
         """
-        Traitement de calcul de la température.
-        :param kwargs: liste d'arguments de la fonction.
-        :return: NoReturn.
+        Action pour mode de fonctionnement 'Ete'.
+        :return: bool fonctionnement de la pompe: True pour 'on', False pour 'off'.
         """
-        self.logger.debug('Démarrage traitement...')
         mesure_temperature_eau = float(self.get_state(self.args["temperature_eau"]))
         mem_temperature_eau = float(self.get_state(self.args["mem_temp"]))
-        mode_de_fonctionnement = self.get_state(self.args["mode_de_fonctionnement"])
-        arret_force = self.get_state(self.args["arret_force"])
-        pompe = self.args["cde_pompe"]
-        pivot = self.get_state(self.args["h_pivot"])
         coef = float(self.get_state(self.args["coef"])) / 100
         mode_calcul = self.get_state(self.args["mode_calcul"])
         periode_filtration = self.args["periode_filtration"]
+        pivot = self.get_state(self.args["h_pivot"])
 
         # Flag FIN_TEMPO
         self.logger.debug(f"Flag fin tempo: {self.fin_tempo}")
@@ -259,76 +288,103 @@ class FiltrationPiscine(hass.Hass):
         else:
             temperature_eau = mem_temperature_eau
 
+        temps_filtration = (self.duree_abaque(temperature_eau) if mode_calcul == 'on'
+                            else self.duree_classique(temperature_eau)) * coef
+
+        self.logger.debug(f"Température Eau: {temperature_eau} / Coéficient de filtration: {coef}")
+        self.logger.debug(f"Durée de Filtration (Mode {'Abaque' if mode_calcul == 'on' else 'Classique'}): "
+                          f"{temps_filtration} h")
+
+        # conversion en timedelta pour simplifier la vie
+        h_temps_filtration = timedelta(hours=temps_filtration)
+        h_mi_temps_filtration = h_temps_filtration / 2
+
+        # Calcul des heures de début et fin filtration en fonction
+        # du temps de filtration avant et après l'heure pivot
+        h_pivot = timedelta(hours=int(pivot[:2]), minutes=int(pivot[3:5]))
+
+        h_debut = h_pivot - h_mi_temps_filtration
+        # Limitation de la fin de filtration à 23:59:59
+        h_max = timedelta(hours=23, minutes=59, seconds=59)
+        h_fin = min(h_pivot + h_mi_temps_filtration, h_max)
+
+        self.logger.info(f"Mi temps: {h_mi_temps_filtration}/Total Filtration: {h_temps_filtration}")
+        self.logger.info(f"h_debut: {h_debut}/h_pivot: {h_pivot}/h_fin: {h_fin}")
+        # Affichage plage horaire
+        affichage_texte = f"{str(h_debut).zfill(8)[:5]}/{str(h_pivot).zfill(8)[:5]}/{str(h_fin).zfill(8)[:5]}"
+        self.set_textvalue(periode_filtration, affichage_texte)
+        # Ajouté le 30 mai 2022
+        self.set_value("input_number.duree_filtration_ete", round(temps_filtration, 2))
+        # fin ajout
+        # Marche pompe si dans plage horaire sinon Arret
+        marche_pompe: bool = self.now_is_between(str(h_debut), str(h_fin))
+        return marche_pompe
+
+    def mode_hiver(self) -> bool:
+        """
+        Action pour mode de fonctionnement 'Hiver'.
+        :return: bool fonctionnement de la pompe: True pour 'on', False pour 'off'.
+        """
+        h_debut_h = self.get_state(self.args["h_debut_hiver"])
+        h_debut_t = timedelta(hours=int(h_debut_h[:2]), minutes=int(h_debut_h[3:5]), seconds=int(h_debut_h[6:8]))
+
+        duree = self.get_state(self.args["duree_hiver"])
+        duree_t = timedelta(hours=duree)
+
+        h_fin = h_debut_t + duree_t
+        # Affichage plage horaire
+        periode_filtration = self.args["periode_filtration"]
+        affichage_texte = f"{str(h_debut_h).zfill(8)[:5]}/{str(h_fin).zfill(8)[:5]}"
+        self.set_textvalue(periode_filtration, affichage_texte)
+
+        self.logger.debug(f"h_debut_h: {h_debut_t}, Duree H: {duree_t}, H_fin: {h_fin}")
+        # Marche pompe si dans plage horaire sinon Arret
+        marche_pompe: bool = self.now_is_between(str(h_debut_h), str(h_fin))
+        return marche_pompe
+
+    def mode_at_force(self) -> bool:
+        """
+        Action pour mode de fonctionnement 'At F' (Arrêt Forcé).
+        :return: bool False pour le flag marche_pompe.
+        """
+        periode_filtration = self.args["periode_filtration"]
+        self.set_textvalue(periode_filtration, "At manuel")
+        return False
+
+    def mode_ma_force(self) -> bool:
+        """
+        Action pour mode de fonctionnement 'Ma F' (Marche Forcée).
+        :return: bool True pour le flag marche_pompe.
+        """
+        periode_filtration = self.args["periode_filtration"]
+        self.set_textvalue(periode_filtration, "Ma manuel")
+        return True
+
+    def traitement(self, kwargs):
+        """
+        Traitement de calcul de la température.
+        :param kwargs: liste d'arguments de la fonction.
+        :return: NoReturn.
+        """
+        self.logger.debug('Démarrage traitement...')
+        mode_de_fonctionnement = self.get_state(self.args["mode_de_fonctionnement"])
         self.logger.debug(f"Mode de fonctionnement: {mode_de_fonctionnement}")
-        #  Mode Ete
-        if mode_de_fonctionnement == TAB_MODE[0]:
-            temps_filtration = (self.duree_abaque(temperature_eau) if mode_calcul == 'on'
-                                else self.duree_classique(temperature_eau)) * coef
-
-            self.logger.debug(f"Température Eau: {temperature_eau} / Coéficient de filtration: {coef}")
-            self.logger.debug(f"Durée de Filtration (Mode {'Abaque'if mode_calcul == 'on' else 'Classique'}): "
-                              f"{temps_filtration} h")
-
-            # conversion en timedelta pour simplifier la vie
-            h_temps_filtration = timedelta(hours=temps_filtration)
-            h_mi_temps_filtration = h_temps_filtration / 2
-
-            # Calcul des heures de début et fin filtration en fonction
-            # du temps de filtration avant et après l'heure pivot
-            h_pivot = timedelta(hours=int(pivot[:2]), minutes=int(pivot[3:5]))
-
-            h_debut = h_pivot - h_mi_temps_filtration
-            # Limitation de la fin de filtration à 23:59:59
-            h_max = timedelta(hours=23, minutes=59, seconds=59)
-            h_fin = min(h_pivot + h_mi_temps_filtration, h_max)
-
-            self.logger.info(f"Mi temps: {h_mi_temps_filtration}/Total Filtration: {h_temps_filtration}")
-            self.logger.info(f"h_debut: {h_debut}/h_pivot: {h_pivot}/h_fin: {h_fin}")
-            # Affichage plage horaire
-            affichage_texte = f"{str(h_debut).zfill(8)[:5]}/{str(h_pivot).zfill(8)[:5]}/{str(h_fin).zfill(8)[:5]}"
-            self.set_textvalue(periode_filtration, affichage_texte)
-            # Ajouté le 30 mai 2022
-            self.set_value("input_number.duree_filtration_ete", round(temps_filtration, 2))
-            # fin ajout
-            # Marche pompe si dans plage horaire sinon Arret
-            marche_pompe: bool = self.now_is_between(str(h_debut), str(h_fin))
-
-        #  Mode hiver Heure de Début + Une durée en h 
-        elif mode_de_fonctionnement == TAB_MODE[1]:
-            h_debut_h = self.get_state(self.args["h_debut_hiver"])
-            h_debut_t = timedelta(hours=int(h_debut_h[:2]), minutes=int(h_debut_h[3:5]), seconds=int(h_debut_h[6:8]))
-
-            duree = self.get_state(self.args["duree_hiver"])
-            duree_t = timedelta(hours=duree)
-
-            h_fin = h_debut_t + duree_t
-            # Affichage plage horaire
-            affichage_texte = f"{str(h_debut_h).zfill(8)[:5]}/{str(h_fin).zfill(8)[:5]}"
-            self.set_textvalue(periode_filtration, affichage_texte)
-
-            self.logger.debug(f"h_debut_h: {h_debut_t}, Duree H: {duree_t}, H_fin: {h_fin}")
-            # Marche pompe si dans plage horaire sinon Arret
-            marche_pompe = self.now_is_between(str(h_debut_h), str(h_fin))
-
-        # Mode Arret Forcé
-        elif mode_de_fonctionnement == TAB_MODE[2]:
-            marche_pompe = False
-            self.set_textvalue(periode_filtration, "At manuel")
-
-        # Mode Marche Forcée
-        elif mode_de_fonctionnement == TAB_MODE[3]:
-            marche_pompe = True
-            self.set_textvalue(periode_filtration, "Ma manuel")
-
-        # Mode Inconnu: revoir le contenu de Input_select.mode_de_fonctionnement
-        else:
+        #  Recupération du mode de fonctionnement
+        marche_pompe: bool = False
+        try:
+            action: Callable[[], bool] = self.get_mode_action(mode_de_fonctionnement)
+            marche_pompe = action()
+        except UnknownModeError:
             self.logger.warning(f"Mode de fonctionnement Piscine Inconnu: {mode_de_fonctionnement}")
 
         # Calcul sortie commande pompe filtration
         # Arrêt pompe sur arrêt forcé
+        arret_force = self.get_state(self.args["arret_force"])
+        pompe = self.args["cde_pompe"]
         if arret_force == "on":
             self.turn_off(pompe)
             self.logger.info("Arrêt par Deléstage (Forcé)")
+            periode_filtration = self.args["periode_filtration"]
             self.set_textvalue(periode_filtration, "At delestage")
         else:
             if marche_pompe:
